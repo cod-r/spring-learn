100cb049fd8de08e16cb112139a56e06b209dee2
capodabo <71343016+capodabo@users.noreply.github.com>
2020-10-28 06:54:27 +0000

    Test create new person and new address

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import com.example.demo.entity.Address;
+import com.example.demo.entity.Person;
+import com.example.demo.repository.AddressRepository;
+import com.example.demo.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired PersonRepository personRepository;
+
 	@Test
-	void contextLoads() {
+	void addPerson() {
+		Person ionel = new Person();
+		ionel.setFirstName("Ionel");
+		ionel.setLastName("Popescu");
+		ionel.setAge(25L);
+		ionel.setGender("male");
+
+		personRepository.save(ionel);
 	}
 
+	@Autowired AddressRepository addressRepository;
+
+	@Test
+	void addAddress() {
+		Address address = new Address();
+		address.setStreetName("Jiul");
+		address.setStreetNumber(6L);
+		address.setFk_person(1L);
+
+		addressRepository.save(address);
+	}
 }
